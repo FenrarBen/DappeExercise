30036e5f29ebd58b55dced1fb60c71e54b398df6
BenStrawbridge <lute_midnight@yahoo.com>
2020-12-06 06:50:19 -0600

    Dapper Exercise

---
+++ DapperExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
